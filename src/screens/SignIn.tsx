fa359d9333cfa17244f31fc48bc37e07f39afd7a
Fernando Rodrigues Coelho <fernando.ufv14@gmail.com>
2023-01-30 15:26:02 -0300

    Heading

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,6 @@
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
-import { VStack, Image, Center, Text } from 'native-base';
+import { VStack, Image, Center, Text, Heading } from 'native-base';
 
 export function SignIn() {
   return (
@@ -18,6 +18,12 @@ export function SignIn() {
         <Text color="gray.100" fontSize="sm">
           Treine sua mente e o seu corpo
         </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" size="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
       </Center>
     </VStack>
   );
